14b876bed710c7363eaee85791adac787c140bb0
GowthamEswar <gowthameswar8@gmail.com>
2025-06-24 13:38:27 +0530

    Add app/loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div style={{ minHeight: "60vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <span style={{ fontFamily: 'Bebas Neue, cursive', fontSize: '2em', color: 'var(--primary-dark)' }}>Loading…</span>
+    </div>
+  );
+}
